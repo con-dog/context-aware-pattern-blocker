fff3085062b6ebe5958b423693fb9b737e860ce7
Papillon <75226598+con-dog@users.noreply.github.com>
2024-11-24 10:43:56 +1300

    Style fixes for shift to sidepanel

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,11 @@
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === 'openSidePanel') {
-    // This will open the panel in all the pages on the current window.
-    chrome.sidePanel.open({ windowId: tab.windowId });
-  }
+chrome.action.onClicked.addListener(async (tab) => {
+  chrome.sidePanel.setOptions(
+    {
+      enabled: true,
+      path: 'side-panel.html',
+    },
+    () => {
+      chrome.sidePanel.open({windowId: tab.windowId});
+    }
+  );
 });
