71d0328e2ce61951256b7f79f59a016e982bba66
Papillon <75226598+con-dog@users.noreply.github.com>
2024-11-28 19:02:18 +1300

    button colour change

--- src/components/create-rule-dialog.tsx
+++ src/components/create-rule-dialog.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
+import { RuleDialog } from "./rule-dialog";
 import { Button } from "./ui/button";
 import { Dialog, DialogTrigger } from "./ui/dialog";
-import { RuleDialog } from "./rule-dialog";
 
 export const CreateRuleDialog: React.FC = () => {
 	const [open, setOpen] = useState(false);
@@ -9,7 +9,7 @@ export const CreateRuleDialog: React.FC = () => {
 	return (
 		<Dialog open={open} onOpenChange={setOpen}>
 			<DialogTrigger asChild>
-				<Button variant="outline">Add new rule</Button>
+				<Button variant="default">Add new rule</Button>
 			</DialogTrigger>
 			<RuleDialog mode="create" open={open} onOpenChange={setOpen} />
 		</Dialog>
