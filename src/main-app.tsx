69c55a2790a1e483fc10bf1b40255b7c4dedbdc7
Papillon <75226598+con-dog@users.noreply.github.com>
2024-11-29 12:40:02 +1300

    add some icons

--- src/main-app.tsx
+++ src/main-app.tsx
@@ -1,3 +1,4 @@
+import { BrainCircuit, Shield, Sliders } from "lucide-react";
 import type React from "react";
 import { useEffect } from "react";
 import { columns } from "./components/columns";
@@ -61,28 +62,50 @@ const App: React.FC = () => {
 							<CardContent className="space-y-4">
 								<div className="grid gap-4 md:grid-cols-3">
 									<Card className="p-4">
-										<h3 className="mb-2 text-base font-base">
+										<h3 className="flex items-center justify-start mb-2 text-base font-medium">
+											<BrainCircuit className="inline-block w-6 h-6 mr-2" />
 											Context-Aware Filtering
 										</h3>
 										<p className="text-sm text-muted-foreground">
-											Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-											Sed do eiusmod tempor incididunt ut labore.
+											Block words or phrases while specifying contexts eg:
+											'Politics' or 'War'. AI will{" "}
+											<strong>
+												<em>unblock</em>
+											</strong>{" "}
+											content if it doesn't match your specified contexts.
 										</p>
 									</Card>
 									<Card className="p-4">
-										<h3 className="mb-2 text-base font-medium">Custom Rules</h3>
+										<h3 className="flex items-center justify-start mb-2 text-base font-medium">
+											<Sliders className="inline-block w-6 h-6 mr-2" />
+											Custom Rules
+										</h3>
 										<p className="text-sm text-muted-foreground">
-											Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-											Ut enim ad minim veniam.
+											Use{" "}
+											<strong>
+												<em>regex</em>
+											</strong>{" "}
+											patterns to block specific words, phrases, or complex text
+											patterns. Choose blocking{" "}
+											<strong>
+												<em>modes</em>
+											</strong>{" "}
+											to block individual matches or the entire surrounding
+											sentence.
 										</p>
 									</Card>
 									<Card className="p-4">
-										<h3 className="mb-2 text-base font-medium">
+										<h3 className="flex items-center justify-start mb-2 text-base font-medium">
+											<Shield className="inline-block w-6 h-6 mr-2" />
 											Real-time Protection
 										</h3>
 										<p className="text-sm text-muted-foreground">
-											Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-											Duis aute irure dolor.
+											Automatically blocks content as soon as it appears, on any
+											webpage. Works{" "}
+											<strong>
+												<em>efficiently</em>
+											</strong>{" "}
+											without impacting your normal browsing experience.
 										</p>
 									</Card>
 								</div>
